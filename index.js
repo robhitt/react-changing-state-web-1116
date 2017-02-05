384d288015c2c4cc96968f75c7cd1bb91bd615fa
Rob Hitt <robhitt@gmail.com>
2017-02-05 15:05:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much.";
+
+class BlogPost extends React.Component {
+  render () {
+    return (
+      <article>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.body}</p>
+      </article>
+    );
+  }
+}
+
+// class Blog extends React.Component {
+//   render () {
+//     return (
+//       <div>
+//         <BlogPost title={'Hello World!'} body={'Hello, this is my blog.'} />
+//         <BlogPost title={'Good bye!'} body={'I\'m busy. I\'m shutting this blog down.'} />
+//       </div>
+//     );
+//   }
+// }
+
+class Blog extends React.Component {
+  componentDidMount () {
+    fetch('http://localhost:4000/api/posts')
+      .then(response => response.json())
+      .then(posts => {
+          console.log('Yey! I got some posts!', posts);
+      });
+  }
+
+
+class App extends React.Component {
+  // ...
+  render () {
+    return (
+      <div>
+        <Modal isOpen={this.state.isModalOpen} />
+      </div>
+    );
+  }
+}
